2e426c71eb2fb608843c65bae13a6f39118afa32
ivikee7 <80277577+ivikee7@users.noreply.github.com>
2023-03-09 22:03:52 +0530

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,9 +18,9 @@ class UserSeeder extends Seeder
     {
         User::create([
             'title'=>'Mr',
-            'first_name' => 'System',
+            'first_name' => 'Super',
             'middle_name' => '',
-            'last_name' => '_',
+            'last_name' => 'Admin',
             'contact_number'=>'1234567890',
             'address_line1'=>'Patna',
             'city'=>'Patna',
@@ -34,7 +34,7 @@ class UserSeeder extends Seeder
             'status'=>1,
             'created_by'=>1,
             'updated_by'=>1,
-            'email' => 'user@domain.local',
+            'email' => 'superadmin@domain.local',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10)
